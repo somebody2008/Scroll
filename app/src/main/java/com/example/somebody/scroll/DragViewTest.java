c9fb15d376366f517a641dfca06ddc5ad2731eee
somebody <348181210@qq.com>
2016-03-15 19:19:36 +0800

    Initial commit -- scroll

---
+++ app/src/main/java/com/example/somebody/scroll/DragViewTest.java
@@ -0,0 +1,15 @@
+package com.example.somebody.scroll;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by somebody on 2016/3/15.
+ */
+public class DragViewTest extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drag_view);
+    }
+}
